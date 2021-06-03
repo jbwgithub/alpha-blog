744dfb0f7db1c6e41c4a54914571dfb9a3412e13
Josh Wintraub <joshbw4@gmail.com>
2021-06-03 16:52:51 -0400

    Added show all articles feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
+
   def show
-    # byebug # Can you to open a debugger console
     @article = Article.find(params[:id]) # the '@' makes it an instance variable, which is necessary to make the variable (in this case an article, available in the the 'show' view)
   end
+
+  def index
+    @articles = Article.all
+  end
+
 end
