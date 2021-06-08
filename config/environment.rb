175eb0d972de2a967bc5d6f6449bf799ae672b8a
Josh Wintraub <joshbw4@gmail.com>
2021-06-08 15:21:05 -0400

    Styled validation error messages, and flash messages

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Added in Section 5 to prevent Rails from creating a new 'errors message' div when a validation fails, as this throws off the styling
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
