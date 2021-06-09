92516250cde6d03c63845a43153a3f91ea5d5df0
Josh Wintraub <joshbw4@gmail.com>
2021-06-09 17:01:49 -0400

    Added new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] # Can list routes individually like this in order to limit exposed routes, but since now using all routes, can use next line
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
