1083d55ff05f295abb56c79e8c90fd8366c68571
Josh Wintraub <joshbw4@gmail.com>
2021-06-10 18:20:48 -0400

    Added user validations for actions on articles and user profiles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     # @article = Article.find(params[:id]) # Removed for refactoring
@@ -54,5 +56,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description) # called whitelisting
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own articles"
+      redirect_to @article
+    end
   end
 end
